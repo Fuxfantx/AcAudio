06be75ea5a46ca0f1fb86932c2a03d4b8de320aa
Fux <109365408+Fuxfantx@users.noreply.github.com>
2024-03-08 17:19:31 +0800

    Trim the miniaudio lib

--- src/ext.cpp
+++ src/ext.cpp
@@ -1,23 +1,40 @@
 /* Aerials Audio System */
 #pragma once
-#ifdef DM_PLATFORM_IOS   // Avoid iOS Runtime Linking
-#define MA_NO_RUNTIME_LINKING
+#define MINIAUDIO_IMPLEMENTATION
+
+/* Trimming the miniaudio Library */
+#define MA_NO_FLAC
+#define MA_NO_ENCODING
+#define MA_NO_GENERATION
+#define MA_NO_RESOURCE_MANAGER
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
+#define MA_API static
+
+#ifdef DM_PLATFORM_WINDOWS
+	#define MA_ENABLE_WASAPI
 #endif
 
+#ifdef DM_PLATFORM_ANDROID
+	#define MA_ENABLE_AAUDIO
+	#define MA_ENABLE_OPENSL
+#endif
 
-/* SDK Includes */
+#ifdef DM_PLATFORM_IOS
+	#define MA_ENABLE_COREAUDIO
+	#define MA_NO_RUNTIME_LINKING   // Avoid iOS Runtime Linking
+#endif
+
+#ifdef DM_PLATFORM_OSX
+	#define MA_ENABLE_COREAUDIO
+	#define MA_NO_RUNTIME_LINKING   // Avoid macOS Runtime Linking
+#endif
+
+/* Includes */
+#include <miniaudio.h>
 #include <dmsdk/sdk.h>
 
 
 /* Lua API Implementations */
-#define MA_NO_FLAC
-#define MA_NO_ENCODING
-#define MA_NO_GENERATION
-#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
-
-
-
 
 
 /* Binding Stuff */
@@ -26,7 +43,7 @@ static const luaL_reg __AcAudio__[] =
 	{0, 0}
 };
 
-inline dmExtension::Result __AcAudioLuaInit__(dmExtension::Params* p) {
+inline dmExtension::Result __AcA_Init__(dmExtension::Params* p) {
 	lua_State* L = p->m_L;
 
 	// Register Modules
@@ -37,12 +54,12 @@ inline dmExtension::Result __AcAudioLuaInit__(dmExtension::Params* p) {
 	return dmExtension::RESULT_OK;
 }
 
-inline dmExtension::Result __AcAudioOK__(dmExtension::Params* params) {
+inline dmExtension::Result __AcA_OK__(dmExtension::Params* params) {
 	return dmExtension::RESULT_OK;
 }
 
-inline dmExtension::Result __AcAudioAPPOK__(dmExtension::AppParams* params) {
+inline dmExtension::Result __AcA_APPOK__(dmExtension::AppParams* params) {
 	return dmExtension::RESULT_OK;
 }
 
-DM_DECLARE_EXTENSION(AcAudio, "AcAudio", __AcAudioAPPOK__, __AcAudioAPPOK__, __AcAudioLuaInit__, nullptr, nullptr, __AcAudioOK__)
+DM_DECLARE_EXTENSION(AcAudio, "AcAudio", __AcA_APPOK__, __AcA_APPOK__, __AcA_Init__, 0, 0, __AcA_OK__)
